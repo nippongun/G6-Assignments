c5530a9d07ac85d8529ff6d678767ae094c77494
simon <simppabauer@gmail.com>
2019-12-09 16:02:45 +0200

    Add mazearray and backup
    mazearray works now with pointers

--- mazearray.c
+++ mazearray.c
@@ -33,60 +33,61 @@
 
 #define RIGHT 0
 #define LEFT 1
+#define xOffset 4
+#define speed 100
+    
+ typedef struct Coordinates{
+        int x;
+        int y;
+    }coordinates;
+typedef enum Direction{WEST=0,NORTH=1,EAST=2, SOUTH=3}direction;
 
-enum Direction{WEST=0,NORTH=1,EAST=2, SOUTH=3}direction;
+void tank_turn( int dir, int delay);
+void r90(struct sensors_ *dig, coordinates *cor,enum Direction *dir,int *check);
+void l90 (struct sensors_ *dig, coordinates *cor,enum Direction *dir,int *check);
+void checkObject(int ultra_array[], int *object);
+void turn(enum Direction *dir, coordinates *cor, int (* maze)[9], struct sensors_ *dig,int *check);
+void add_object(int *object,direction *dir,coordinates *cor, int (* maze)[9]);
 
-struct Coordinates{
-    int x;
-    int y;
-}coordinates;
-
-void tank_turn(uint8_t speed, int dir, int delay);
-void r90();
-void l90();
-void backup();
-void checkObject();
-void forward();
-void turn();
-void add_object();
-const int xOffset = 4;
-const int yOffset = 0;
-int maze[16][9]={
-    {1,1,1,1,0,1,1,1,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,0,0,0,0,0,0,0,1},
-    {1,1,0,0,0,0,0,1,1},
-    {1,1,1,0,0,0,1,1,1},
-    {1,1,1,1,0,1,1,1,1},
-};
-
-enum Direction direction = NORTH;
-
-int check = 0;
-int object = 0;
-int counter = 0;
-int aux_count = 0;
-int object_count = 0;
-const int speed = 100;
-
-TickType_t start, end;
-
-
-struct sensors_ dig;
-int ultra_array[5];
 void zmain(){
-    coordinates.x = 0;
-    coordinates.y = -2;
+    int maze[16][9]={
+        {1,1,1,1,0,1,1,1,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1},
+        {1,1,0,0,0,0,0,1,1},
+        {1,1,1,0,0,0,1,1,1},
+        {1,1,1,1,0,1,1,1,1},
+    };
+
+
+
+    int check = 0;
+    int object = 0;
+    //uint8_t speed = 100;
+
+    TickType_t start = 0, end = 0;
+
+
+    struct sensors_ dig;
+    int ultra_array[5];
+
+    
+    direction dir = NORTH;
+   
+    
+    coordinates cor;
+    cor.x = 0;
+    cor.y = -2;
     
     IR_Start();
     motor_start();
@@ -99,7 +100,7 @@ void zmain(){
     }
     while(1){
                 
-        checkObject();
+        checkObject(ultra_array,&object);
         reflectance_digital(&dig);
         // Line Following
         // This makes sure the robot will follow the line among the intersections
@@ -116,35 +117,31 @@ void zmain(){
         } 
         // Main Part
         // THis checks for the intersections
-        if(((dig.l3 == 1) && (dig.r3 == 1)&& (check == 0)) || (coordinates.x == 3 && (dig.l3 == 1) && direction == NORTH && (dig.r3 == 0))||(coordinates.x == -3 && (dig.r3 == 1) && direction == NORTH &&(dig.l3 == 0) )){
+        if(((dig.l3 == 1) && (dig.r3 == 1)&& (check == 0)) || (cor.x == 3 && (dig.l3 == 1) && dir == NORTH && (dig.r3 == 0))||(cor.x == -3 && (dig.r3 == 1) && dir == NORTH &&(dig.l3 == 0) )){
             check = 1;
             start = xTaskGetTickCount();   
-            if(direction == NORTH){
-                coordinates.y++;
-            } else if (direction == EAST){
-                coordinates.x++;
-            } else if(direction == WEST){
-                coordinates.x--;
-            } else if(direction == SOUTH){
-                coordinates.y--;
+            if(dir == NORTH){
+                cor.y++;
+            } else if (dir == EAST){
+                cor.x++;
+            } else if(dir == WEST){
+                cor.x--;
+            } else if(dir == SOUTH){
+                cor.y--;
             }
-            print_mqtt("Zumo061/","X:%d  Y:%d  DIR:%d",coordinates.x,coordinates.y,direction); 
+            print_mqtt("Zumo061/","X:%d  Y:%d  DIR:%d",cor.x,cor.y,cor); 
         // THis part makes sure the bot is on a line in between intersections    
         } else if((dig.l3 == 0) && (dig.r3 == 0) && (check != 0)){
             end = xTaskGetTickCount();
             motor_forward(speed,end - start);
-            counter++;
             
-            
-            // If a
-            add_object();
-            turn();
-            checkObject();
-            add_object();
-            turn();
-            turn();
-            
-                        
+            add_object(&object,&dir,&cor,maze);
+            turn(&dir,&cor,maze,&dig,&check);
+            checkObject(ultra_array,&object);
+            add_object(&object,&dir,&cor,maze);
+            turn(&dir,&cor,maze,&dig,&check);
+            turn(&dir,&cor,maze,&dig,&check);
+                          
             check = 0;
         }
         motor_forward(speed,0);
@@ -154,61 +151,63 @@ void zmain(){
 
 // r90 and l90 are behaving the same.
 // First, the bot moves until the specific inner sensors read white. From there on, the bot continues until it reads black again
-void r90(){
-    while(dig.r1 != 0 ||dig.l1 != 0 ){
-        tank_turn(speed,RIGHT,1);
-        reflectance_digital(&dig);
+void r90( struct sensors_ *dig, coordinates *cor,enum Direction *dir, int *check){
+    while(dig->r1 != 0 ||dig->l1 != 0 ){
+        tank_turn(RIGHT,0);
+        reflectance_digital(dig);
     }
                 
     motor_forward(0,500);
     
-    while(dig.r1 != 1){
-        tank_turn(speed,RIGHT,1);
-        reflectance_digital(&dig);
+    while(dig->r1 != 1){
+        tank_turn(RIGHT,0);
+        reflectance_digital(dig);
     }
     motor_forward(0,500);
     
-    if(coordinates.x > -3 && coordinates.x <= 0){
-        while(dig.r1 != 0){
-            tank_turn(speed,RIGHT,1);
-            reflectance_digital(&dig);
+    if(cor->x > -3 && cor->x <= 0){
+        while(dig->r1 != 0){
+            tank_turn(RIGHT,0);
+            reflectance_digital(dig);
         }
     }
     motor_forward(0,500);
-    direction++;
-    check = 0;
-    checkObject();
+    *dir = *dir+1;
+    print_mqtt("Zumo061/","%d",dir);
+    *check = 0;
+    //checkObject();
 }
 
-void l90(){
+void l90( struct sensors_ *dig, coordinates *cor,enum Direction *dir, int *check){
     
     motor_forward(0,500);
-    while(dig.r1 != 0 ||dig.l1 != 0 ){
-        tank_turn(speed,LEFT,1);
-        reflectance_digital(&dig);
+    while(dig->r1 != 0 ||dig->l1 != 0 ){
+        tank_turn(LEFT,0);
+        reflectance_digital(dig);
     }
                 
     motor_forward(0,500);
     
-    while(dig.l1 != 1){
-        tank_turn(speed,LEFT,1);
-        reflectance_digital(&dig);
+    while(dig->l1 != 1){
+        tank_turn(LEFT,0);
+        reflectance_digital(dig);
     }
-    if(coordinates.x < 3 && coordinates.x >= 0){
-        while(dig.l1 != 0){
-            tank_turn(speed,LEFT,1);
-            reflectance_digital(&dig);
+    if(cor->x < 3 && cor->x >= 0){
+        while(dig->l1 != 0){
+            tank_turn(LEFT,0);
+            reflectance_digital(dig);
         }
     }
     motor_forward(0,500);
-    direction--;
-    check = 0;
-    checkObject();
+    *dir = *dir-1;
+    print_mqtt("Zumo061/","%d",dir);
+    *check = 0;
+    //checkObject(int ultra_array[], int );
 }
 
 // By setting the different motor in opposite direction, this allows a cleaner turn in a grid system.
 // While 0 represents forward movement, 1 represents backward movement
-void tank_turn(uint8_t speed, int dir, int delay){
+void tank_turn(int dir, int delay){
     if(dir == 0 ){
         //tank turn right
         MotorDirLeft_Write(0);
@@ -221,139 +220,99 @@ void tank_turn(uint8_t speed, int dir, int delay){
     //Since "motor_turn" does not set the any motor in any direction, this function can be utilized.
     motor_turn(speed,speed,delay);
 }
-
-void backup(){
-    while(dig.r3 != 1 || dig.l3 != 1){
-        reflectance_digital(&dig);
-        
-        while(dig.l1 != 1){
-            //tank_turn(general_speed, RIGHT,1);
-            motor_turn(speed+8,0,1);
-            reflectance_digital(&dig); 
-        }
-        while(dig.r1 != 1){
-            //tank_turn(general_speed, LEFT,1);
-            motor_turn(0,speed,1);
-            reflectance_digital(&dig); 
-        }
-        motor_backward(speed,0);
-    }
-}
-
-void forward(){
-    while((dig.l3 == 0) && (dig.r3 == 0)){
-        motor_forward(speed,0);
-        reflectance_digital(&dig);
-        checkObject();
-    }
-}
 //To avoid false negatives, i.e the sonic sensors don't read objects properly, the reading must be done redundantly.
 //
-void checkObject(){
+void checkObject(int ultra_array[], int *object){
     
     for(int i = 0; i < 5; i++){
             ultra_array[i]=Ultra_GetDistance();
         }
         
-        if(ultra_array[3]<15){
-            object = 1;
-            
-        }
-        
+    if(ultra_array[3]<15){
+        *object = 1;  
+    }     
 }
 
 
-void turn(){
+void turn(enum Direction *dir, coordinates *cor, int (* maze)[9], struct sensors_ *dig,int *check){
     
-     if(direction == WEST) {
+     if(*dir == WEST) {
       
         // if object above and no object left
-        if(maze[coordinates.y+1][coordinates.x+xOffset]==1 && maze[coordinates.y][coordinates.x-1+xOffset] == 0){
+        if(maze[cor->y+1][cor->x+xOffset]==1 && maze[cor->y][cor->x-1+xOffset] == 0){
             // go forward  
             //forward();
-        } else if(maze[coordinates.y+1][coordinates.x+xOffset]== 0 || maze[coordinates.y][coordinates.x-1+xOffset] == 1){
-            r90();
-        } else if(maze[coordinates.y][coordinates.x+xOffset-1]== 1 && maze[coordinates.y+1][coordinates.x+xOffset] == 1){
-            r90();
-            r90();
+        } else if(maze[cor->y+1][cor->x+xOffset]== 0 || maze[cor->y][cor->x-1+xOffset] == 1){
+            r90(dig, cor, dir, check);
+        } else if(maze[cor->y][cor->x+xOffset-1]== 1 && maze[cor->y+1][cor->x+xOffset] == 1){
+            r90(dig, cor, dir, check);
+            r90(dig, cor, dir, check);
             motor_stop();
         }
        
     }
-    if(direction == EAST) {
+    if(*dir == EAST) {
       
         // if object above and no object right
         
-        if(maze[coordinates.y+1][coordinates.x+xOffset]==1 && maze[coordinates.y][coordinates.x+1+xOffset] == 0){
+        if(maze[cor->y+1][cor->x+xOffset]==1 && maze[cor->y][cor->x+1+xOffset] == 0){
         
         //
-        } else if(maze[coordinates.y+1][coordinates.x+xOffset]== 0 || maze[coordinates.y][coordinates.x+1+xOffset] == 1){
-            l90();
+        } else if(maze[cor->y+1][cor->x+xOffset]== 0 || maze[cor->y][cor->x+1+xOffset] == 1){
+            l90(dig, cor, dir, check);
             
         /* ▓
         *  >▓  
         */    
         // if an object above and right of the bot
-        } else if(maze[coordinates.y][coordinates.x+xOffset+1]== 1 && maze[coordinates.y+1][coordinates.x+xOffset] == 1){
+        } else if(maze[cor->y][cor->x+xOffset+1]== 1 && maze[cor->y+1][cor->x+xOffset] == 1){
             // go left twice
-            l90();
-            l90();
+            l90(dig, cor, dir, check);
+            l90(dig, cor, dir, check);
         }
        
     }
     
-    if(direction == NORTH){
+    if(*dir == NORTH){
         // if left and right of the bot
-        if(maze[coordinates.y+yOffset][coordinates.x+xOffset+1] && maze[coordinates.y+yOffset][coordinates.x+xOffset-1] && maze[coordinates.y+yOffset+1][coordinates.x+xOffset]== 0  && direction==NORTH){
+        if(maze[cor->y][cor->x+xOffset+1] && maze[cor->y][cor->x+xOffset-1] && maze[cor->y+1][cor->x+xOffset]== 0){
             //go forward
             //forward();
         // if an object ahead and the block next to it blocking the way, and the intersect on the right upper corner is open, go right
-        } else if(maze[coordinates.y+1][coordinates.x+xOffset-1]&&maze[coordinates.y+1][coordinates.x+xOffset]&& maze[coordinates.y+1][coordinates.x+xOffset+1] == 0){
+        } else if(maze[cor->y+1][cor->x+xOffset-1]&&maze[cor->y+1][cor->x+xOffset]&& maze[cor->y+1][cor->x+xOffset+1] == 0){
             // ho right
-            r90();
+            r90(dig, cor, dir, check);
         // if in front and right 
-        } else if (maze[coordinates.y+yOffset+1][coordinates.x+xOffset] &&maze[coordinates.y+yOffset][coordinates.x+1+xOffset] && direction == NORTH){
+        } else if (maze[cor->y+1][cor->x+xOffset] &&maze[cor->y][cor->x+1+xOffset] ){
             // go left
-            l90();
+            l90(dig, cor, dir, check);
             //motor_stop();
         // if in front and left    
-        } else if(maze[coordinates.y+yOffset+1][coordinates.x+xOffset]&&maze[coordinates.y+yOffset][coordinates.x+xOffset-1]){
+        } else if(maze[cor->y+1][cor->x+xOffset]&&maze[cor->y][cor->x+xOffset-1]){
             // go right
-            r90();
+            r90(dig, cor, dir, check);
             //motor_stop();
             // if in front is occupied and left is free, 
-        } else if(maze[coordinates.y+yOffset+1][coordinates.x+xOffset]==1 && maze[coordinates.y+yOffset][coordinates.x+xOffset-1] == 0){
+        } else if(maze[cor->y+1][cor->x+xOffset]==1 && maze[cor->y][cor->x+xOffset-1] == 0){
             // turn left
-            l90();
-        } else if(maze[coordinates.y+1][coordinates.x+xOffset]==0){
+            l90(dig, cor, dir, check);
+        } else if(maze[cor->y+1][cor->x+xOffset]==0){
             //forward();
         }
     }
-    
-   
-
 }
 
-void add_object(){
-    if(object){
-        //Beep(250,1);
+void add_object(int *object,direction *dir,coordinates *cor, int (* maze)[9]){
+    if(*object == 1){
         print_mqtt("Zumo061/","Object");
-        object_count++;               
-        counter = 0;
-        aux_count = 1;
-        if(direction == NORTH){
-            maze[coordinates.y+1+yOffset][coordinates.x+xOffset] = 1;
-            //l90();
-        } else if (direction == EAST){
-            maze[coordinates.y+yOffset][coordinates.x+1+xOffset] = 1;
-            //l90();
-        } else if(direction == WEST){
-            maze[coordinates.y+yOffset][coordinates.x-1+xOffset] = 1;
-            //r90();
-        }else{
-            //r90();
+        if(*dir == NORTH){
+            maze[cor->y+1][cor->x+xOffset] = 1;
+        } else if (*dir == EAST){
+            maze[cor->y][cor->x+1+xOffset] = 1;
+        } else if(*dir == WEST){
+            maze[cor->y][cor->x-1+xOffset] = 1;
         }
-        object = 0;
+        *object = 0;
     } 
 }
 
